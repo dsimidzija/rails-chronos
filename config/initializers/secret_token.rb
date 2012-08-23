ae26885f20a7b8b4292440a5871005cbc631cb68
Dusko Simidzija <dusko.simidzija@gmail.com>
2012-08-23 19:47:54 +0200

    initial rails Skeletor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chronos::Application.config.secret_token = 'e767ec5ebe726fcaa0e3571cd6f07b0bf0b99ca61b72885458b2b25dc8c387e96fe0f447a53c7889500bf300523923feb88cd146b41729965a48804b6ff966e8'
