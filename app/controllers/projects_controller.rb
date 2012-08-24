a32c2d4f5cee968c40b56fe752caf112ec0498bf
Dusko Simidzija <dusko.simidzija@gmail.com>
2012-08-24 15:23:14 +0200

    added creating new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,19 @@
 class ProjectsController < ApplicationController
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(params[:project])
+
+    if not @project.save
+      flash.now[:notice] = 'Could not save the project'
+      render :action => 'new'
+      return
+    end
+
+    flash[:success] = 'Project saved'
+    redirect_to user_projects_path(@current_user)
   end
 
   def edit
