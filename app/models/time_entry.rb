6d36ae6384eb880faff5dcad5fb90489d7cb3a02
Dusko Simidzija <dusko.simidzija@gmail.com>
2012-08-27 23:26:01 +0200

    added invalid range validation on time entry

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -4,13 +4,21 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  attr_accessible :project_id, :entry_date_formatted, :start_time_formatted, :end_time_formatted, :note
+  attr_accessible :project_id,
+                  :entry_date_formatted,
+                  :start_time_formatted,
+                  :end_time_formatted,
+                  :note
 
   validates_presence_of   :user_id, :project_id, :entry_date_formatted, :start_time_formatted
   validate                :start_time_before_end_time, :if => :both_timestamps_present?
+
   validates_format_of     :entry_date_formatted, :with => /^\d{2}\.\d{2}\.\d{4}$/
   validates_format_of     :start_time_formatted, :with => /^\d{2}:\d{2}$/
-  validates_format_of     :end_time_formatted, :with => /^\d{2}:\d{2}$/, :if => :both_timestamps_present?
+  validates_format_of     :end_time_formatted, :with => /^\d{2}:\d{2}$/,
+                          :if => :both_timestamps_present?
+
+  validate                :check_input_ranges
 
   def entry_date_formatted
     self.entry_date.strftime date_input_format unless self.entry_date.nil?
@@ -18,7 +26,12 @@ class TimeEntry < ActiveRecord::Base
 
   def entry_date_formatted=(value)
     return if value.nil? or value.blank?
-    self.entry_date = DateTime.strptime(value, date_input_format)
+
+    begin
+      self.entry_date = DateTime.strptime(value, date_input_format)
+    rescue
+      add_input_range_error(:entry_date)
+    end
   end
 
   def start_time_formatted
@@ -27,7 +40,12 @@ class TimeEntry < ActiveRecord::Base
 
   def start_time_formatted=(value)
     return if value.nil? or value.blank?
-    self.start_time = DateTime.strptime(value, time_input_format)
+
+    begin
+      self.start_time = DateTime.strptime(value, time_input_format)
+    rescue
+      add_input_range_error(:start_time)
+    end
   end
 
   def end_time_formatted
@@ -41,7 +59,11 @@ class TimeEntry < ActiveRecord::Base
       return
     end
 
-    self.end_time = DateTime.strptime(value, time_input_format)
+    begin
+      self.end_time = DateTime.strptime(value, time_input_format)
+    rescue
+      add_input_range_error(:end_time)
+    end
   end
 
   def time_in_hours
@@ -76,6 +98,20 @@ class TimeEntry < ActiveRecord::Base
 
   def start_time_before_end_time
     errors.add(:end_time, 'cannot be before start time') unless end_time > start_time
+  end
+
+  def check_input_ranges
+    return if @input_range_errors.nil?
+
+    @input_range_errors.each do |key, value|
+      errors.add(key.to_sym, value)
+    end
+    @input_range_errors = {}
+  end
+
+  def add_input_range_error(field)
+    @input_range_errors = Hash.new if @input_range_errors.nil?
+    @input_range_errors[field] = 'contains invalid values'
   end
 
 end
