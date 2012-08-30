0698112f894e7e995aa2a01921b3f404709069a0
Dusko Simidzija <dusko.simidzija@gmail.com>
2012-08-30 11:25:12 +0200

    more css tweaks

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,10 +2,10 @@ module ReportsHelper
   def css_class_for_percent(percent, date)
     return '' if [0,6].include?(date.wday)
 
-    return 'label label-important' if percent < 0.95
-    return 'label label-warning' if (0.95..0.99).include?(percent)
-    return 'label label-success' if percent >= 1.1
-    'label label-info'
+    return 'total_time time_insufficient' if percent < 0.95
+    return 'total_time time_warning' if (0.95..0.99).include?(percent)
+    return 'total_time time_success' if percent >= 1.1
+    'total_time time_ok'
   end
 
   def css_class_for_empty_cell(percent)
