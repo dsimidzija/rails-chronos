0ac1b21989983565e8a02e97e4c2296b309cc71c
Dusko Simidzija <dusko.simidzija@gmail.com>
2012-08-31 15:19:36 +0200

    fixed tracking target time

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -1,11 +1,15 @@
 class TrackingController < ApplicationController
   before_filter :login_required
   before_filter :user_allowed_access?
+  before_filter :find_tracking_entries, :only => [:dashboard]
+  before_filter :find_daily_entries, :only => [:dashboard]
 
   def dashboard
-    find_tracking_entries
     @tracking = Tracking.new unless @time_entries.any?
     @tracking = @time_entries.first if @time_entries.any?
+
+    @first_entry = @tracking
+    @first_entry = @daily_entries.first if @daily_entries.any?
   end
 
   def start
@@ -44,6 +48,17 @@ class TrackingController < ApplicationController
         :end_time => nil,
         :user_id => @current_user.id
       },
-      :order => { :start_time => :asc })
+      :order => { :start_time => :asc }
+    )
+  end
+
+  def find_daily_entries
+    @daily_entries = TimeEntry.find(:all,
+      :conditions => {
+        :entry_date => Date.today,
+        :user_id => @current_user.id
+      },
+      :order => { :start_time => :asc }
+    )
   end
 end
